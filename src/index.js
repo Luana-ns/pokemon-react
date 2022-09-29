5e3b752b88a627615b6f568e0f802da2b7e3b9f1
ogatalars <ogataogara@gmail.com>
2022-09-29 19:28:47 -0300

    Some refactoring, add POKE_API, also change some files

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Home from './pages/Home';
-import reportWebVitals from './performance/reportWebVitals';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Home />
+    <App />
   </React.StrictMode>
 );
 
